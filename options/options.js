fa1f875886b7197812ba3d256024633854fa5413
Crowdbotics <team@crowdbotics.com>
2024-05-11 22:19:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-meadow-47879",
+  url: "https://holy-meadow-47879.botics.co",
+  api: "https://holy-meadow-47879.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
